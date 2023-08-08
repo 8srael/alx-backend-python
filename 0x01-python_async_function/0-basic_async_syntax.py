8b62efc46db2e92fd2f6d44da6232260f8b7ef6a
8srael <kouassiisrael36@gmail.com>
2023-08-08 23:12:03 +0000

    Task 0, 🅰sync basics

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+"""Waits for a random number of seconds"""
 
 import asyncio
 import random
 
+
 async def wait_random(max_delay=10):
+    """Waits for a random number of seconds."""
     random_number = random.uniform(0, max_delay)
     await asyncio.sleep(random_number)
     return random_number
