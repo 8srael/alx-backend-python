ed6c71c8caa663fc39e1ac14b581b706796a3549
8srael <kouassiisrael36@gmail.com>
2023-08-09 14:48:24 +0000

    Task 1. Execution of multiple coroutines✅

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,5 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawn wait_random n times with the specified max_delay."""
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    return [await task for task in asyncio.as_completed(tasks)]
+    delays = await asyncio.gather(*[wait_random(max_delay) for _ in range(n)])
+
+    return sorted(delays)
+
