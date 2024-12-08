d1fd68040b30288f5128730b6df110d008eb35bd
8srael <kouassiisrael36@gmail.com>
2024-12-08 16:02:14 +0000

    Task 3. Using decorators to retry database queries✅

--- python-decorators-0x01/2-transactional.py
+++ python-decorators-0x01/2-transactional.py
@@ -1,40 +1,39 @@
-import sqlite3 
+import sqlite3
 import functools
-
 
 def with_db_connection(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         connection = sqlite3.connect('users.db')
-        result = func(connection, *args, **kwargs)
-        connection.close()
-        return result
+        kwargs['connection'] = connection
+        try:
+            return func(*args, **kwargs)
+        finally:
+            connection.close()
     return wrapper
 
-"""your code goes here"""
 def transactional(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         connection = kwargs.get('connection')
         if connection is None:
             raise ValueError("No DB connection found")
+        connection.execute('BEGIN')
         try:
             result = func(*args, **kwargs)
             connection.commit()
+            return result
         except Exception as e:
             connection.rollback()
             raise e
-        finally:
-            connection.close()
-        return result
     return wrapper
 
+@with_db_connection
+@transactional
+def update_user_email(connection, user_id, new_email):
+    cursor = connection.cursor()
+    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id))
 
-@with_db_connection 
-@transactional 
-def update_user_email(conn, user_id, new_email): 
-    cursor = conn.cursor() 
-    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id)) 
-    
-#### Update user's email with automatic transaction handling 
+# Test de mise à jour
 update_user_email(user_id=1, new_email='Crawford_Cartwright@hotmail.com')
+print("User email updated successfully!")
