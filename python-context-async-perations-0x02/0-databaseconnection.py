71b6e5a60db5fad6e188b70c354b16c59a980d7b
8srael <kouassiisrael36@gmail.com>
2024-12-10 17:43:36 +0000

    Tasl 0. custom class based context manager for Database connection✅

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+class DatabaseConnection():
+    """
+        Context manager class for database connection
+    """
+    def __enter__(self):
+        self.conn = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="ALX_prodev"
+        )
+        
+        return self.conn
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.conn.close()
+        if exc_type is not None:
+            return False
+        return True
+    
+# Usage
+
+if __name__ == "__main__":
+    with DatabaseConnection() as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM user_data")
+        for row in cursor.fetchall():
+            print(row)
---
+++ python-context-async-perations-0x02/README.md
@@ -0,0 +1,3 @@
+## Context Managers and Asynchronous programming in python
+
+*Task 0* : Custom class based context manager for Database connection
