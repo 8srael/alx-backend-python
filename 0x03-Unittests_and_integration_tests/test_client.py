2d507947a604b79399dba9d933c5e2d5a46eaa5e
8srael <kouassiisrael36@gmail.com>
2024-12-16 16:17:48 +0000

    Task 6. More patching🆗

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -42,3 +42,22 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_org.return_value = {"repos_url": "http://test.com"}
             client = GithubOrgClient("test")
             self.assertEqual(client._public_repos_url, "http://test.com")
+            
+    @patch('client.get_json', return_value=[{"name": "repo1"}, {"name": "repo2"}])            
+    def test_public_repos(self, mock_get_json) -> None:
+        """
+            Test that the public_repos method returns the correct value.
+        """
+        mock_url = "http://api.github.com/orgs/google/repos"
+        with patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock, return_value=mock_url) as mock_public_repos_url:
+            client = GithubOrgClient('google')
+            repos = client.public_repos()
+           
+            # We expect the method to return ['repo1', 'repo2'] based on the mocked payload
+            self.assertEqual(repos, ['repo1', 'repo2'])
+
+            # Check that _public_repos_url was accessed once
+            mock_public_repos_url.assert_called_once()
+
+            # Check that get_json was called once with the mocked URL
+            mock_get_json.assert_called_once_with(mock_url)
