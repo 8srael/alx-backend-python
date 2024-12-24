ee4c32d085f3db48d5e89a89dd2895d5c37ff4b8
8srael <kouassiisrael36@gmail.com>
2024-12-24 21:44:30 +0000

    set up mysql database✅

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -84,10 +84,15 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
     }
 }
+
 
 
 # Password validation
